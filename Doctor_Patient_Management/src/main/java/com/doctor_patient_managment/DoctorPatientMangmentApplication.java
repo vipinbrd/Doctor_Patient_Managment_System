f2dda583412d733f96118edca22372718ded2986
Vipinbrd <vipinbarode7@gmail.com>
2023-01-26 13:10:37 +0530

    dependencies added

---
+++ Doctor_Patient_Management/src/main/java/com/doctor_patient_managment/DoctorPatientMangmentApplication.java
@@ -0,0 +1,13 @@
+package com.doctor_patient_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientMangmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientMangmentApplication.class, args);
+	}
+
+}
---
+++ Doctor_Patient_Management/src/test/java/com/doctor_patient_managment/DoctorPatientMangmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor_patient_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPatientMangmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
