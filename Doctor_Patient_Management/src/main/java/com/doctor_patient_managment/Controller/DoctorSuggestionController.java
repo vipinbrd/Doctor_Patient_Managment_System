43709a74810f21c5abe3c545f4b6cc7f3ed11b7d
Vipinbrd <vipinbarode7@gmail.com>
2023-01-26 19:58:25 +0530

    DoctorSuggestion Service Implementation Added

--- Doctor_Patient_Management/src/main/java/com/doctor_patient_managment/Controller/DoctorSuggestionController.java
+++ Doctor_Patient_Management/src/main/java/com/doctor_patient_managment/Controller/DoctorSuggestionController.java
@@ -1,6 +1,9 @@
 package com.doctor_patient_managment.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.doctor_patient_managment.Exceptions.DoctorNotFoundException;
 import com.doctor_patient_managment.Exceptions.PatientNotFoundException;
+import com.doctor_patient_managment.Model.Doctor;
 import com.doctor_patient_managment.ServiceIMPL.SuggestingDoctorServiceImpl;
 
 @RestController
@@ -17,12 +21,12 @@ public class DoctorSuggestionController {
 	@Autowired
 	private SuggestingDoctorServiceImpl suggestionService;
 	
+	@GetMapping("/doctors/{patientId}")
+  public ResponseEntity<List<Doctor>> getAllSuggestedDoctor(@PathVariable("patientId") Integer id) throws DoctorNotFoundException, PatientNotFoundException{
 	
-	@GetMapping("/suggest/{id}")
-	public String checking(@PathVariable("id") Integer id) throws DoctorNotFoundException, PatientNotFoundException {
-		
-		suggestionService.searchDoctorByPatientId(id);
-		return "ok";
-	}
-
+	
+		List<Doctor>  doctors = suggestionService.searchDoctorByPatientId(id);
+	
+	return new ResponseEntity<List<Doctor>>(doctors,HttpStatus.OK);
+}
 }
--- Doctor_Patient_Management/src/main/java/com/doctor_patient_managment/ServiceIMPL/SuggestingDoctorServiceImpl.java
+++ Doctor_Patient_Management/src/main/java/com/doctor_patient_managment/ServiceIMPL/SuggestingDoctorServiceImpl.java
@@ -1,7 +1,11 @@
 package com.doctor_patient_managment.ServiceIMPL;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,29 +20,82 @@ import com.doctor_patient_managment.Services.SuggestingDoctorService;
 
 @Service
 public class SuggestingDoctorServiceImpl implements SuggestingDoctorService {
-@Autowired
-private DoctorRepository doctorRepo;
-@Autowired
-private PatientRepository patientRepo;
+	@Autowired
+	private DoctorRepository doctorRepo;
+	@Autowired
+	private PatientRepository patientRepo;
+
 	@Override
 	public List<Doctor> searchDoctorByPatientId(Integer patientId)
 			throws DoctorNotFoundException, PatientNotFoundException {
-	
-		
-	Optional<Patient>opt=	patientRepo.findById(patientId);
-	if(opt.isEmpty())throw new PatientNotFoundException("patient not found for given id");
 
-	 Patient patient=opt.get();
-	 String symtoms=patient.getSymtom();
-	 String location=patient.getCity();
-	 System.out.println(symtoms);
-	 System.out.println(location);
-	
+		Optional<Patient> opt = patientRepo.findById(patientId);
+		if (opt.isEmpty())
+			throw new PatientNotFoundException("patient not found for given id");
+
+		Patient patient = opt.get();
+		String symtoms = patient.getSymtom();
+		String location = patient.getCity();
 		
-	
 		
 		
-		return null;
+
+		List<Doctor> doctorList = doctorRepo.findAll();
+		
+		
+//getting all doctors of patient's location
+		List<Doctor> filteredListBasedOnLacation = doctorList.stream()
+				.filter(doctor -> (doctor.getCity().toString().equalsIgnoreCase(location)))
+				.collect(Collectors.toList());
+		if (filteredListBasedOnLacation.size() == 0) {
+			throw new DoctorNotFoundException("We are still waiting to expand to your location");
+		}
+		
+		
+
+		List<Doctor> specialistsBasedOnPatientSymtoms = new ArrayList<>();
+
+
+
+		for (Doctor doctor : filteredListBasedOnLacation) {
+			String speciality = doctor.getSpeciality().toString();
+
+			if (speciality.equalsIgnoreCase("Orthopedic")) {
+				if (symtoms.equalsIgnoreCase("Arthritis") || symtoms.equalsIgnoreCase("Backpain")
+						|| symtoms.equalsIgnoreCase("Tissue injuries")) {
+
+					specialistsBasedOnPatientSymtoms.add(doctor);
+				}
+
+			}
+
+			else if (speciality.equalsIgnoreCase("Gynecology") && symtoms.equalsIgnoreCase("Dysmenorrhea")) {
+				specialistsBasedOnPatientSymtoms.add(doctor);
+
+			} else if (speciality.equalsIgnoreCase("Dermatology")) {
+				if (symtoms.equalsIgnoreCase("Skin infection") || symtoms.equalsIgnoreCase("skin burn")) {
+
+					specialistsBasedOnPatientSymtoms.add(doctor);
+				}
+
+			} else {
+				if (speciality.equalsIgnoreCase("ENT") && symtoms.equalsIgnoreCase("Ear pain")) {
+					specialistsBasedOnPatientSymtoms.add(doctor);
+
+				}
+
+			}
+
+		}
+		
+		if(specialistsBasedOnPatientSymtoms.size()==0) {
+			throw new DoctorNotFoundException("There isn’t any doctor present at your location for your symptom");}
+		
+//		System.out.println(specialistsBasedOnPatientSymtoms);
+		
+		
+		return  specialistsBasedOnPatientSymtoms;
 	}
+	
 
 }
